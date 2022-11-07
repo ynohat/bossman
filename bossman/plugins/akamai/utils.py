f7e75acdb8f8fbaf47a3b951e535f9936d7c3b03
wooshcz <wooshcz@gmail.com>
2022-11-07 22:04:21 +0100

    truncate the description field

--- bossman/plugins/akamai/utils.py
+++ bossman/plugins/akamai/utils.py
@@ -7,7 +7,7 @@ from bossman.repo import Revision
 
 class GenericVersionComments:
   @staticmethod
-  def from_revision(revision: Revision):
+  def from_revision(revision: Revision, truncate_to: int = None):
     comments = StringIO()
     comments.write(revision.message.strip())
     comments.write("\n\n")
@@ -16,9 +16,11 @@ class GenericVersionComments:
     comments.write("author: {} <{}>\n".format(revision.author_name, revision.author_email))
     if revision.author_email != revision.committer_email:
       comments.write("committer: {} <{}>\n".format(revision.commit.committer.name, revision.commit.committer.email))
-    return GenericVersionComments(comments.getvalue())
+    else:
+      return GenericVersionComments(comments.getvalue(), truncate_to)
 
-  def __init__(self, comments: str):
+  def __init__(self, comments: str, truncate_to: int = None):
+    self.truncate_to = truncate_to
     parts = comments.strip().rsplit("\n\n", 1)
     self.message = parts[0]
     self.metadata = (OrderedDict(re.findall(r"^([^:]+):\s*(.*)$", parts[1], re.MULTILINE))
@@ -26,13 +28,26 @@ class GenericVersionComments:
       else {})
 
   def __str__(self) -> str:
-    return "{}\n\n{}".format(
+    if self.truncate_to:
+      if len(" ".join(self.metadata.keys() + self.metadata.values())) > self.truncate_to:
+        # if the total length of the keys and values in the metadata dictionary is greater
+        # than the truncate_to limit, we'll shorten the description field by removing 
+        # the 'branch' information from metadata
+        self.metadata.move_to_end("branch")
+        self.metadata.popitem()
+    out = "{}\n\n{}".format(
       self.message,
       "\n".join(
         "{}: {}".format(k, v)
         for k, v in self.metadata.items()
       )
     )
+    if self.truncate_to:
+      # this explicitly truncates the string to the set limit to avoid triggering the API 
+      # validation errors -- no useful information should be lost in the process
+      return out[:self.truncate_to]
+    else:
+      return out
 
   @property
   def commit(self):
